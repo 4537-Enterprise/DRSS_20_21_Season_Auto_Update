f3f171cc0c3cd6cab6d7363441ac6cf595c72ea2
Antoine Gagne <gagne.3@daytonstemschool.org>
2020-12-20 13:03:10 -0500

    Removed Testing Thingy For a test ;)
    Signed-off-by: DRSS Enterprise <drss.enterprise@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Code/Webcam/webcamTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Code/Webcam/webcamTest.java
@@ -24,7 +24,6 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
 
 @TeleOp(name = "Webcam test", group = "Test")
 public class webcamTest extends LinearOpMode{
-	//test
 
 	private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
 	private static final boolean PHONE_IS_PORTRAIT = false  ;
