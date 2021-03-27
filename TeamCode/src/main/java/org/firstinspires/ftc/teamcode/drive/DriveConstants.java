4a8883aa0372becfc361c323dd05d1d26437f377
DRSS Enterprise <drss.enterprise@gmail.com>
2021-03-27 13:59:22 -0400

    Changed Road runner PID Variables
    Signed-off-by: DRSS Enterprise <drss.enterprise@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -47,7 +47,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.9685; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 16; // in
+    public static double TRACK_WIDTH = 8.31; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.02;
-    public static double kA = 0.001;
-    public static double kStatic = 0.04002;
+    public static double kV = 0.025;
+    public static double kA = 0.0015;
+    public static double kStatic = 0.11105;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -89,7 +89,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 55.61473667135343;
     public static double MAX_ACCEL = 55.61473667135343;
-    public static double MAX_ANG_VEL = Math.toRadians(199.155605625);
+    public static double MAX_ANG_VEL = Math.toRadians(246.9076227792149);
     public static double MAX_ANG_ACCEL = Math.toRadians(199.155605625);
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -60,10 +60,10 @@ import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(12, 0, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(13.5, 0, 0);
 
-    public static double LATERAL_MULTIPLIER = 1;
+    public static double LATERAL_MULTIPLIER = 1.6;
 
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
